f672cc242d7d81f632716105e28a6dc04dc661d0
Brady Metherall <brady.metherall@uoit.net>
2017-02-10 11:06:35 -0500

    Updated the force of the black hole.

--- BlackHoleEquation.py
+++ BlackHoleEquation.py
@@ -11,8 +11,8 @@ class BlackHole(Equation):
         d_aw[d_idx] = 0.0
 
     def loop(self, d_x, d_y, d_z, d_idx, d_au, d_av, d_aw, t):
-        d_au[d_idx] += d_x[d_idx] / pow((d_x[d_idx]*d_x[d_idx] + d_y[d_idx]*d_y[d_idx] + d_z[d_idx]*d_z[d_idx] + self.plum*self.plum + (d_z[d_idx] - t)*(d_z[d_idx] - t)),3.0/2.0)
-        d_av[d_idx] += d_y[d_idx] / pow((d_x[d_idx]*d_x[d_idx] + d_y[d_idx]*d_y[d_idx] + d_z[d_idx]*d_z[d_idx] + self.plum*self.plum+ (d_z[d_idx] - t)*(d_z[d_idx] - t)),3.0/2.0)
+        d_au[d_idx] += 4*d_x[d_idx] / pow((d_x[d_idx]*d_x[d_idx] + d_y[d_idx]*d_y[d_idx] + d_z[d_idx]*d_z[d_idx] + self.plum*self.plum + (d_y[d_idx] - t)*(d_y[d_idx] - t)),3.0/2.0)
+        d_av[d_idx] += 4*(2*d_y[d_idx] - t) / pow((d_x[d_idx]*d_x[d_idx] + d_y[d_idx]*d_y[d_idx] + d_z[d_idx]*d_z[d_idx] + self.plum*self.plum+ (d_y[d_idx] - t)*(d_y[d_idx] - t)),3.0/2.0)
         d_aw[d_idx] += 0
 
 
