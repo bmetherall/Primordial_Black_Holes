62d95b4e309b489e186b83a492b583f47b8a47c0
Brady Metherall <brady.metherall@uoit.net>
2017-02-13 15:47:54 -0500

    Update the accelerations, I used the spherical 'r' instead of the cylindrical.

--- BlackHoleEquation.py
+++ BlackHoleEquation.py
@@ -14,7 +14,7 @@ class BlackHole(Equation):
 
     # calculate the force due to the black hole
     def loop(self, d_x, d_y, d_z, d_idx, d_au, d_av, d_aw, t):
-        d_au[d_idx] += -self.M*d_x[d_idx] / pow((d_x[d_idx]*d_x[d_idx] + d_y[d_idx]*d_y[d_idx] + d_z[d_idx]*d_z[d_idx] + self.soft*self.soft + (d_y[d_idx] - t + self.t_hit)*(d_y[d_idx] - t + self.t_hit)),3.0/2.0)
-        d_av[d_idx] += -self.M*(2*d_y[d_idx] - t + self.t_hit) / pow((d_x[d_idx]*d_x[d_idx] + d_y[d_idx]*d_y[d_idx] + d_z[d_idx]*d_z[d_idx] + self.soft*self.soft + (d_y[d_idx] - t + self.t_hit)*(d_y[d_idx] - t + self.t_hit)),3.0/2.0)
+        d_au[d_idx] += -self.M*d_x[d_idx] / pow((d_x[d_idx]*d_x[d_idx] + d_z[d_idx]*d_z[d_idx] + self.soft*self.soft + (d_y[d_idx] - t + self.t_hit)*(d_y[d_idx] - t + self.t_hit)),3.0/2.0)
+        d_av[d_idx] += -self.M*(2*d_y[d_idx] - t + self.t_hit) / pow((d_x[d_idx]*d_x[d_idx] + d_z[d_idx]*d_z[d_idx] + self.soft*self.soft + (d_y[d_idx] - t + self.t_hit)*(d_y[d_idx] - t + self.t_hit)),3.0/2.0)
         d_aw[d_idx] += 0
 
